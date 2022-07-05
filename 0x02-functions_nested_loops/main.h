26825f47871f227a34c0d4ae2b9a0b9d7a5a0a2d
=getnetAdane <=getnetadane075@gmail.com>
2022-07-05 11:56:02 -0700

    main commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar (char c);
+void print _alphabet(void);
+void print _alphabet_x10(void);
+int islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_baver(void);
+void times_table(void);
+int add(int,int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
