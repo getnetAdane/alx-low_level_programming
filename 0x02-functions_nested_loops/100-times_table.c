436ce962d25fad1dc71971ae2f3b09ef1e91e4ad
getnetAdane <48225096+getnetAdane@users.noreply.github.com>
2022-07-06 08:38:32 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_time_table- prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-void print_time_table(int n)
+void print_times_table(int n)
 {
 int i, j, k;
 if (n >= 0 && n <= 15)
