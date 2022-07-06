8cbac8fea320aa925287720d486786c96ca65795
getnetAdane <48225096+getnetAdane@users.noreply.github.com>
2022-07-06 16:39:01 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,63 +1,33 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * largest_number - returns the largest of 3 numbers
- *
- *   * @a: first integer
- *
- *    * @b: second integer
- *
- *     * @c: third integer
- *
- *      * Return: largest number
- *
- *       */
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
 
 int largest_number(int a, int b, int c)
-
 {
+	int largest;
 
-		int test;
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-			int largest;
-
-
-
-				if (a > b)
-
-						{
-
-									test = a;
-
-										}
-
-					else
-
-							{
-
-										test = b;
-
-											}
-
-						if (test > c)
-
-								{
-
-											largest = test;
-
-												}
-
-							else
-
-									{
-
-												largest = c;
-
-													}
-
-								return (largest);
-
+return (largest);
 }
