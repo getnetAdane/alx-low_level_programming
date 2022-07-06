de3951287b045205c4a4084d1055cc72d8f7e88b
getnetAdane <48225096+getnetAdane@users.noreply.github.com>
2022-07-06 23:35:00 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,17 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int integer;
-	for (integer = '0' ; integer <= '9' ; integer++)
-	{
-		putchar(integer);
-	}
-	putchar('\n');
-	return (0);
+int integer;
+for (integer = '0' ; integer <= '9' ; integer++)
+{
+putchar(integer);
+}
+putchar('\n');
+return (0);
 }
