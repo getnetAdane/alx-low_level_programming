e87803519f4fb8920454aa7d885de42818aabd37
getnetAdane <48225096+getnetAdane@users.noreply.github.com>
2022-07-08 17:17:31 +0300

    Update 1-main.c

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,35 +1,18 @@
 #include "mains.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code for Holberton School students.
  *
- *  * main - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+    char c;
 
-	    char c;
-
-
-
-	        c = '0';
-
-		    printf("%c: %d\n", c, _isdigit(c));
-
-		        c = 'a';
-
-			    printf("%c: %d\n", c, _isdigit(c));
-
-			        return (0);
-
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
 }
